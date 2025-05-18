7ef1fcf85ed368a22c9ee266959cff0ac2010d7b
j1v4n200 <testerserver15@gmail.com>
2025-05-18 10:28:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
+    API_ID = environ.get("API_ID", "20406918")
+    API_HASH = environ.get("API_HASH", "45a7cdec1a4177ffde664c2639dfa203")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7521220766:AAFxzVgfjKJHY0RX_Rc8nPvEzHqgwUWWRio") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://telegram:telegram@secondobot.ytc81fz.mongodb.net/?retryWrites=true&w=majority&appName=secondobot")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
 
